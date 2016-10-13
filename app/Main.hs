98745e58d78004f8afe6169dbaeceadde8cce819
Justus Adam <dev@justus.science>
2016-10-13 11:15:01 +0200

    It compiles

--- app/Main.hs
+++ app/Main.hs
@@ -1,2 +1,6 @@
 module Main where
     
+import ClassyPrelude
+
+main :: IO ()
+main = return ()
--- src/LessonBuilder.hs
+++ src/LessonBuilder.hs
@@ -1,24 +1,56 @@
-{-# LANGUAGE NamedFieldPuns  #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns   #-}
+{-# LANGUAGE TemplateHaskell  #-}
+{-# LANGUAGE TypeFamilies #-}
 module LessonBuilder where
 
 
-import           ClassyPrelude
+import           ClassyPrelude              hiding (async)
+import           Control.Concurrent.Async
+import           Control.Monad.Except (ExceptT(..), runExceptT, throwError)
 import           Crypto.MAC.HMAC
+import Crypto.Hash.Algorithms
+import Crypto.Hash
 import           Data.Aeson
+import           Data.Aeson.TH
 import           Data.Aeson.Types
 import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.ByteString.Char8 as BS
+import           Network.HTTP.Types
 import           Network.HTTP.Types.Header
 import           Network.URI
 import           Network.Wai
-import           System.Logger.Log
+import           System.Directory
+import           System.Exit
+import           System.FilePath
+import           System.Log.Logger
+import           System.Process
+import           Text.Printf
+import qualified Data.Foldable as F
 
 
-type BuildConf = HashMap String Build
+data Include = Include
+    { includeRepository :: String
+    , includeDirectory  :: FilePath
+    }
+
+
+data BuildConf = BuildConf
+    { builds   :: HashMap String Build
+    , includes :: [Include]
+    }
 
 
 defaultDataDirectory :: FilePath
 defaultDataDirectory = ".data"
+
+
+buildConfigName :: FilePath
+buildConfigName = "build_conf.json"
+
+
+skipStrings :: [String]
+skipStrings = ["[skip build]", "[build skip]"]
 
 
 additionalCommandOptions :: HashMap String [String]
@@ -62,14 +94,20 @@ data Watch = Watch
 
 
 data WatchConf = WatchConf
-    { dataDirectory :: FilePath
-    , watched       :: [Watch]
-    , secret        :: Maybe ByteString
+    { dataDirectory  :: Maybe FilePath
+    , watched        :: [Watch]
+    , secret         :: Maybe String
+    , reposDirectory :: FilePath
     }
 
-data Event a
+
+data CommitData = CommitData { commitMessage :: String }
+
+
+data Event
     = Push
-        { eventRepo :: Repo
+        { eventRepo       :: Repo
+        , eventHeadCommit :: CommitData
         }
     | Ping
         { eventRepo :: Repo
@@ -91,75 +129,145 @@ instance FromJSON Repo where
             <*> o .: "id"
             <*> o .: "url"
 
+instance ToJSON Repo where
+    toJSON Repo{repoName, repoId, apiUrl} = object ["full_name".=repoName, "id".=repoId, "url".=apiUrl]
+
 
 let dropPrefix p t = fromMaybe t $ stripPrefix p t
     prefixOpts prefix = defaultOptions { fieldLabelModifier = camelTo2 '_' . dropPrefix prefix } in
-    deriveJSON (prefixOpts "build") ''Build
-    deriveJSON (prefixOpts "event") ''Event
-    deriveJSON (prefixOpts "watch") ''Watch
-    deriveJSON defaultOptions { fieldLabelModifier = camelTo2 '_' } ''WatchConf
+    join <$> sequence
+        [ deriveJSON (prefixOpts "commit") ''CommitData
+        , deriveJSON (prefixOpts "include") ''Include
+        , deriveJSON (prefixOpts "build") ''Build
+        , deriveJSON (prefixOpts "buildConf") ''BuildConf
+        , deriveJSON (prefixOpts "event") ''Event
+        , deriveJSON (prefixOpts "watch") ''Watch
+        , deriveJSON defaultOptions { fieldLabelModifier = camelTo2 '_' } ''WatchConf
+        ]
 
 
-buildIt :: Build -> String -> IO Bool
+type LBuilder = ExceptT String IO
+
+
+buildIt :: Build -> String -> LBuilder ()
 buildIt Build{command, targetDir, sourceDir} file = do
-    exists <- do
-        isDir <- doesDirectoryExist targetDir
-        if isDir
-            then return True
-            else do
-                isFile <- doesFileExist targetDir
-                if isFile
-                    then do
-                        errorM "worker" $ printf "Target directory '%s' is file" targetDir
-                        return False
-                    else do
-                        createDirectory targetDir
-                        return True
+    isDir <- liftIO $ doesDirectoryExist targetDir
+    unless isDir $ do
+        isFile <- liftIO $ doesFileExist targetDir
+        if isFile
+            then throwError $ printf "Target directory '%s' is file" targetDir
+            else liftIO $ createDirectory targetDir
 
-    when exists $ do
-        res <- lastMay <$> replicate repeats (readCreateProcessWithExitCode process "")
-        case res of
-            Nothing -> do
-                errorM "worker" $ printf "Unexpected number of repeats: %d" repeats
-                return False
-            Just (ExitSuccess, _, _) -> return True
-            Just (code, stdout, stderr) -> do
-                errorM "worker" $ printf "Build failed with %d for command %s (%d repeats)" code command repeats
-                errorM "worker" "------stderr------"
-                errorM "worker" stderr
-                errorM "worker" "------stdout------"
-                errorM "worker" stdout
-                return False
+    res <- lastMay <$> (replicateM repeats (liftIO $ readCreateProcessWithExitCode process "") :: LBuilder [(ExitCode, String, String)])
+    case res of
+        Nothing -> throwError $ printf "Unexpected number of repeats: %d" repeats
+        Just (ExitSuccess, _, _) -> return ()
+        Just (ExitFailure i, stdout, stderr) -> do
+            liftIO $ errorM "worker" $ printf "Build failed with %d for command %s (%d repeats)\n------stderr------\n%s------stdout------\n%s\n" i command repeats stderr stdout
+            throwError $ stderr
   where
-    repeats = fromMaybe 1 $ lookup repeatsMap command
-    process = proc command (["-output-directory", targetDir] ++ fromMaybe [] (lookup command additionalCommandOptions)) { cwd = Just sourceDir }
+    repeats = fromMaybe 1 $ lookup command repeatsMap
+    process = (proc command (["-output-directory", targetDir] ++ fromMaybe [] (lookup command additionalCommandOptions))) { cwd = Just sourceDir }
 
 
-buildAll :: Build -> IO [Async Bool]
-buildAll b = mapM (async . buildIt b) (files b)
+asyncBuilder :: LBuilder a -> LBuilder (Async (Either String a))
+asyncBuilder = liftIO . async . runExceptT
+
+
+waitBuilder :: Async (Either String a) -> LBuilder a
+waitBuilder = ExceptT . wait 
+
+
+buildAll :: Build -> LBuilder [Async (Either String ())]
+buildAll b = mapM (asyncBuilder . buildIt b) (files b)
+
+
+waitForBuilders :: Foldable f => f (Async (Either String ())) -> LBuilder ()
+waitForBuilders = F.traverse_ waitBuilder
+
+
+runBuildersAndWait :: Traversable f => f (LBuilder ()) -> LBuilder ()
+runBuildersAndWait = traverse asyncBuilder >=> waitForBuilders
 
 
 verifyUAgent :: WatchConf -> ByteString -> Request -> IO Bool
-verifyUAgent WatchConf{secret = Nothing} _ request = True
-verifyUAgent WatchConf{secret = Just secret'} payload headers =
+verifyUAgent WatchConf{secret = Nothing} _ request = return True
+verifyUAgent WatchConf{secret = Just secret'} payload request =
     case (,) <$> lookup "signature" (requestHeaders request) <*> requestHeaderUserAgent request of
         Just (signature, userAgent)
             | "Github-Hookshot/" `isPrefixOf` userAgent ->
-                maybe (err "Uncomputable digest") (return . (== digestFromByteString signature)) computed
-        Nothing -> do
-            err "Missing header"
-            return False
-        _ -> do
-            err "Wrong user agent"
-            return False
+                case digestFromByteString signature of
+                    Nothing -> err "Uncomputable digest"
+                    Just expected | expected == computed -> return True
+                    _ -> err "Digests do not match"
+        Nothing -> err "Missing header"
+        _ -> err "Wrong user agent"
   where
-    err = errorM "verify"
-    computed = hmacGetDigest $ hmac secret' payload :: Digest SHA1
+    err msg = errorM "verification" msg >> return False
+    computed = hmacGetDigest $ hmac (BS.pack secret') payload :: Digest SHA1
 
 
-handleEvent :: Event -> IO ()
-handleEvent Ping{} = errorM "worker" "Ping event should not be handeled in worker"
-handleEvent Push{} = return ()
+gitRefresh :: String -> FilePath -> LBuilder ()
+gitRefresh url targetDir = do
+    exists <- liftIO $ doesDirectoryExist targetDir
+    let process = if exists
+                  then proc "git" ["clone", url, targetDir]
+                  else proc "git" ["-C", targetDir, "pull"]
+    (code, stdout, stderr) <- liftIO $ readCreateProcessWithExitCode process ""
+    case code of
+        ExitSuccess -> return ()
+        ExitFailure c -> throwError $ "Git process failed with" ++ stdout
+
+
+makeInclude :: Include -> LBuilder ()
+makeInclude Include{includeRepository, includeDirectory} = do
+    gitRefresh includeRepository includeDirectory
+    buildProject includeDirectory
+
+
+buildProject :: FilePath -> LBuilder ()
+buildProject directory = do
+    raw <- readFile $ directory </> buildConfigName
+    buildConf <- either (const $ throwError "Unreadable build configuration") return $ eitherDecode raw
+
+    let relativeIncludes = map (\i -> i {includeDirectory = directory </> includeDirectory i}) (includes buildConf)
+
+    runBuildersAndWait $ map makeInclude relativeIncludes
+
+    let relativeBuilds = map (relativize . snd) $ mapToList (builds buildConf)
+          where relativize build = build { sourceDir = directory </> sourceDir build
+                                         , targetDir = directory </> targetDir build }
+
+    traverse buildAll relativeBuilds >>= waitForBuilders . join
+    return ()
+
+
+repoToUrl :: Repo -> String
+    -- TODO Use host
+repoToUrl Repo{repoName} = "https://github.com/" ++ repoName 
+
+
+handleEvent :: WatchConf -> Event -> IO ()
+handleEvent WatchConf{watched, reposDirectory} = handle
+  where
+    handle Ping{} = errorM "worker" "Ping event should not be handeled in worker"
+    handle Push{eventRepo, eventHeadCommit} = do
+        res <- runExceptT $
+                    -- TODO handle special events (push to own repo)
+                    case lookup (repoName eventRepo) watchMap of
+                        Nothing -> throwError "Unrecognized repository"
+                        _ | any (`isInfixOf` commitMessage eventHeadCommit) skipStrings -> do
+                            liftIO $ infoM "worker" "skipping commit due to skip message"
+                            return ()
+                        Just Watch{directory} -> do
+                            wd <- liftIO $ getCurrentDirectory
+                            makeInclude $ Include 
+                                { includeDirectory = wd </> reposDirectory </> directory
+                                , includeRepository = repoToUrl eventRepo
+                                }
+
+        either (errorM "worker") return res
+    watchMap = mapFromList $ map (watchName &&& id) watched :: HashMap String Watch
 
 
 app :: FilePath -> WatchConf -> Application
@@ -167,7 +275,7 @@ app logLocation watchConf request respond = do
     body <- lazyRequestBody request
     -- If the PING event does not send the signature this verification
     -- needs to be moved to after the payload has been parsed and the event type determined
-    verified <- verifyUAgent watchConf body request
+    verified <- verifyUAgent watchConf (toStrict body) request
     if verified
         then
             case eitherDecode' body of
@@ -189,7 +297,7 @@ app logLocation watchConf request respond = do
                             writeFile filePath $ encode hook
                             respond $ responseLBS ok200 [] $ "Ping received. Data saved in " ++ B.pack filePath
                 Right event -> do
-                    void $ async $ handleEvent p event
+                    void $ async $ handleEvent watchConf event
                     respond $ responseLBS ok200 [] $ "Hook received. For build results refer to the log at " ++ bsLogLoc
         else respond $ responseLBS badRequest400 [] $ "Unverifiable. For details on the error refer to the log at " ++ bsLogLoc
   where
